1673878a333ec77d875c69479bf895a8a2f619ae
Dynesshely <catrol@qq.com>
2023-01-21 13:53:12 +0800

    💾 Feat: Update Base to adapt GitHub Pages

--- default/docs/.vuepress/config.ts
+++ default/docs/.vuepress/config.ts
@@ -13,7 +13,7 @@ export default defineUserConfig({
     lang: 'zh-CN',
     title: 'KitX Docs',
     description: 'KitX 文档站点',
-    //   base: '/docs/'
+    base: '/KitX-Docs/',
     locales: {
         // 键名是该语言所属的子路径
         // 作为特例，默认语言可以使用 '/' 作为其路径。
