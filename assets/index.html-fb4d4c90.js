1c4834c694ff658ec78d1b8df111534703d92ff8
Dynesshely <catrol@qq.com>
2023-01-25 08:52:24 +0000

    Deploying to gh-pages from @ Crequency/KitX-Docs@143c719012baf29623bead19dbe2a9d4fd1295f1 🚀

---
+++ assets/index.html-fb4d4c90.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-5f434467","path":"/mobile/guide/","title":"项目简介","lang":"zh-CN","frontmatter":{"lang":"zh-CN","title":"项目简介","description":"本页简单介绍 KitX Mobile 项目"},"headers":[],"git":{"updatedTime":1674636682000,"contributors":[{"name":"Dynesshely","email":"catrol@qq.com","commits":1}]},"filePathRelative":"mobile/guide/README.md"}');export{e as data};
